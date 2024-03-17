643ffedd2834fb817d518fdc76b44c720d09df05
Christopher Neisen <christopherjay71186@gmail.com>
2024-03-16 21:19:53 -0500

    ui changes done

--- frontend-repo/src/components/chatcomponent.js
+++ frontend-repo/src/components/chatcomponent.js
@@ -1,124 +1,133 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 //import axios from 'axios';
 import OpenAI from "openai";
-import './chatcomponent.css'
-import styled from 'styled-components';
+import "../styles/chatcomponent.css";
+import styled from "styled-components";
 
 const ChatContainer = styled.div`
-  max-width: 600px;
-  margin: auto;
-  padding: 20px;
-  border: 1px solid #ccc;
+  padding: 10px;
+  border: 10px solid #57c4e1;
   border-radius: 8px;
+  margin: 10px;
+  padding-bottom: 60px;
+  background-color: white;
+  max-height: 580px;
+  min-height: 580px;
+  overflow: scroll;
 `;
 
 const MessageContainer = styled.div`
-  margin-bottom: 10px;
+  margin-bottom: 0px;
 
   &.user {
     text-align: right;
     color: #007bff;
-    
   }
 
   &.assistant {
     overflow-y: scroll;
-    
+
     border-right: 2px solid;
     width: 0;
-    animation: typing 1.5s steps(30,  end)
-     forwards, blinking 1s
-    infinite;
+    animation: typing 1.5s steps(30, end) forwards, blinking 1s infinite;
     text-align: left;
     color: #000000;
+    white-space: normal;
   }
 `;
 
 const InputContainer = styled.div`
   display: flex;
-  margin-top: 20px;
+  position: sticky;
+  top: 0;
+  width: 92%;
+  margin-left: 22px;
+  margin-bottom: 20px;
 
   input {
     flex: 1;
     padding: 8px;
     border: 1px solid #ccc;
-    border-radius: 4px;
-    margin-right: 10px;
+    border-top-left-radius: 4px;
+    border-bottom-left-radius: 4px;
+    margin-left: -10px;
+    margin-bottom: -10px;
   }
 
   button {
-    background-color: #007bff;
+    background-color: #57c4e1;
     color: #fff;
     padding: 8px 12px;
     border: none;
-    border-radius: 4px;
+    border-top-right-radius: 4px;
+    border-bottom-right-radius: 4px;
+    margin-bottom: -10px;
     cursor: pointer;
   }
 `;
 
-
 const ChatComponent = () => {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const [messages, setMessages] = useState([]);
 
   const handleInputChange = (e) => {
     setInput(e.target.value);
   };
 
-  
-  
   const handleSendMessage = async () => {
     // Make a request to the ChatGPT API with the user input
- //   const apiKey = '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';
-    const openai = new OpenAI({apiKey: process.env.REACT_APP_OPENAI_API_KEY, dangerouslyAllowBrowser: true})
-    
-   // const endpoint = 'https://api.openai.com/v1/completions';
-    try{
-        // Make a request to the ChatGPT API with the user input using the openai library
+    //   const apiKey = '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';
+    const openai = new OpenAI({
+      apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+      dangerouslyAllowBrowser: true,
+    });
+
+    // const endpoint = 'https://api.openai.com/v1/completions';
+    try {
+      // Make a request to the ChatGPT API with the user input using the openai library
       const chatCompletion = await openai.chat.completions.create({
         messages: [
-            
-          { role: 'system', content: 'You are a helpful assistant.' },
-          { role: 'user', content: input },
+          { role: "system", content: "You are a helpful assistant." },
+          { role: "user", content: input },
         ],
-        model: 'gpt-3.5-turbo',
+        model: "gpt-3.5-turbo",
       });
-        
-     
 
-       // Check if chatCompletion and chatCompletion.choices are defined
-    if (chatCompletion && chatCompletion.choices) {
-
+      // Check if chatCompletion and chatCompletion.choices are defined
+      if (chatCompletion && chatCompletion.choices) {
         // Access the content from the first choice in the array
         const content = chatCompletion.choices[0].message.content;
-        setMessages([...messages, {role: 'assistant', content}]);
-    }
-    else {
-        console.error('chatCompletion or chatCompletion.choices is undefined');
-    }
+        setMessages([...messages, { role: "assistant", content }]);
+      } else {
+        console.error("chatCompletion or chatCompletion.choices is undefined");
+      }
 
-    // Clear the input field
-    setInput('');
-    }
-    catch (error) {
-        console.error('API Error:', error.response.data); // Log the error response
+      // Clear the input field
+      setInput("");
+    } catch (error) {
+      console.error("API Error:", error.response.data); // Log the error response
     }
   };
 
   return (
-    <ChatContainer>
-      <div className='Typed' >
-        {messages.map((message, index) => (
-          <MessageContainer key={index} className={message.role}>
-            {message.content}
-          </MessageContainer>
-        ))}
-      </div>
-      <InputContainer>
+    <>
+      <InputContainer
+        onKeyUp={(e) => {
+          if (e.key === "Enter") handleSendMessage();
+        }}>
         <input type="text" value={input} onChange={handleInputChange} />
         <button onClick={handleSendMessage}>Send</button>
       </InputContainer>
-    </ChatContainer>
+      <ChatContainer>
+        <div className="Typed">
+          {messages.map((message, index) => (
+            <MessageContainer key={index} className={message.role}>
+              {message.content}
+            </MessageContainer>
+          ))}
+        </div>
+      </ChatContainer>
+    </>
   );
 };
 
--- frontend-repo/src/components/jobDescription.js
+++ frontend-repo/src/components/jobDescription.js
@@ -1,32 +1,29 @@
 //import './jobDescription.css';
-import React from 'react'
-import {Worker, Viewer} from '@react-pdf-viewer/core';
-import '@react-pdf-viewer/core/lib/styles/index.css'
-import styled from 'styled-components';
+import React from "react";
+import { Worker, Viewer } from "@react-pdf-viewer/core";
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import styled from "styled-components";
 
-const PDFContainer = styled.div `
-padding:20px;
-border: 10px solid #ccc;
-margin: auto;
-border-radius: 10px;
+const PDFContainer = styled.div`
+  padding: 20px;
+  border: 10px solid #57c4e1;
+  margin: 10px;
+  border-radius: 10px;
+  background-color: white;
 `;
 
-
-
-
-
 const jobDescription = () => {
-    const pdfUrl = '/pdf/SFSoftwareDeveloper.pdf';
-    return (
+  const pdfUrl = "/pdf/SFSoftwareDeveloper.pdf";
+  return (
     <div>
-        <h2>Job Description</h2>
-        <PDFContainer className='scrollable'>
+      <h2 style={{ color: "white" }}>Job Description</h2>
+      <PDFContainer className="scrollable">
         <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
           <Viewer fileUrl={pdfUrl} />
         </Worker>
-        </PDFContainer>
+      </PDFContainer>
     </div>
-    );
+  );
 };
 
 export default jobDescription;
